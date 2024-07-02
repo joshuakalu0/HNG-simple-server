8e13f09ffbdcfd59fb195c851bf34937a5099e49
Aju Joshua kalu <69370514+joshuakalu0@users.noreply.github.com>
2024-07-02 10:09:51 +0100

    Update app.js
    Api

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+var geoip = require("geoip-lite");
+
 const express = require('express');
 
 const app = express()
@@ -7,9 +10,26 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
+app.get("/api/hello", async (req, res) => {
+  const { visitor_name } = req.query;
+
+  const test_ip = "207.97.227.239";
+  const geo = geoip.lookup(req.ip);
+  console.log(geo);
+  api_key = "a4f791ec3190105377dcfdf1cf72f27d";
+  const url = `http://api.openweathermap.org/data/2.5/find?q=${geo.city}&appid=${api_key}`;
+  const response = await axios.get(url);
+  const { temp } = response.data.list[0].main;
+  const c = Math.round(Number(temp) - 273.15);
+
+  console.log(temp);
+
+  res.json({
+    client_ip: ip,
+    location: "",
+    greeting: `Hello ${visitor_name}!, the temperature is ${c} degrees celcius in ${geo.city}`,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
