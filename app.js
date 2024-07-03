f994a6c2126474d99ab7917d0b8dd75527cc717f
Aju Joshua kalu <69370514+joshuakalu0@users.noreply.github.com>
2024-07-03 21:08:53 +0100

    Update app.js
    Hj

--- app.js
+++ app.js
@@ -11,34 +11,35 @@ const PORT = 8000
 
 app.get('/', (req, res) => {
   const ip = req.header('x-forwarded-for') || req.connection.remoteAddress
-  const url =`https://api.ipgeolocation.io/ipgeo?apiKey=55c05094273c478eb9b8a0fa42412c42&ip=${ip}`
   
-  axios.get(url).then(re=>res.json(re.data))
-  //res.send(`Hello World your ip is:${ip}`)
+  res.send(`Hello World your ip is:${ip}`)
 })
+
+
+
 app.get("/api/hello", (req, res) => {
   const { visitor_name } = req.query;
 
   const ip = req.header('x-forwarded-for') || req.connection.remoteAddress
-satelize.satelize({ip:ip}, function(err, payload) {
+  const url =`https://api.ipgeolocation.io/ipgeo?apiKey=55c05094273c478eb9b8a0fa42412c42&ip=${ip}`
+  
 
-      // res.send(payload);
-       res.json(payload)
-
-})
   api_key = "a4f791ec3190105377dcfdf1cf72f27d";
-  const url = `http://api.openweathermap.org/data/2.5/find?q=${geo.city}&appid=${api_key}`;
-//const response = await axios.get(url);
-  //const { temp } = response.data.list[0].main;
- // const c = Math.round(Number(temp) - 273.15)
- //const data =geoip.lookup(ip);
-
-
-  //res.json({
-    //client_ip: ip,
-   // location: "",
-    //greeting: `Hello ${visitor_name}!, the temperature is ${c} degrees celcius in ${geo.city}`,
-  //});
+  
+  axios.get(url).then(geo=>{
+    const url_two = `http://api.openweathermap.org/data/2.5/find?q=${geo.data.city}&appid=${api_key}`;
+    axios.get(url_two).then(response=>{
+      const { temp } = response.data.list[0].main;
+      const c = Math.round(Number(temp) - 273.15)
+      res.json({
+    client_ip: ip,
+   location: `${geo.data.city} ${geo.data.state_prov}, ${geo.data.country_name}`,
+    greeting: `Hello ${visitor_name}!, the temperature is ${c} degrees celcius in ${geo.city}`,
+  });
+      
+    })
+    })
+  
 })
 
 
